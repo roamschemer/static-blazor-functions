c817b88b201b7928b77d5b36e4120ae554005211
roamschemer <roamschemer@gmail.com>
2021-07-05 22:38:42 +0900

    post を作成

--- Api/TempDataFunction.cs
+++ Api/TempDataFunction.cs
@@ -6,6 +6,8 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Data;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace Api {
     public static class TempDataFunction {
@@ -15,10 +17,22 @@ namespace Api {
             ILogger log) {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
+            string id = req.Query["id"];
+            string temp = req.Query["temp"];
+
+            string requestBody = String.Empty;
+            using (StreamReader streamReader = new StreamReader(req.Body)) {
+                requestBody = await streamReader.ReadToEndAsync();
+            }
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            id = id ?? data?.id;
+            temp = temp ?? data?.temp;
+
             var r = new Random(DateTime.Now.Millisecond);
             var tempData = new TempData() {
+                Id = id ?? "none",
                 DateTime = DateTime.Now,
-                Temp = r.Next(0, 1000) * 0.1
+                Temp = temp != null ? double.Parse(temp) : 0
             };
 
             return new OkObjectResult(tempData);
--- Data/TempData.cs
+++ Data/TempData.cs
@@ -2,6 +2,7 @@
 
 namespace Data {
     public class TempData {
+        public string Id { get; set; }
         public DateTime DateTime { get; set; }
         public double Temp { get; set; }
     }
